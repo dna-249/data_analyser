1d989b92c4104a155da23fc6745aad2311fdf876
Your Name <danamonuraalhaji@gmail.com>
2025-12-30 12:54:58 +0100

    commit

--- myapp/app/welcome.py
+++ myapp/app/welcome.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-from .models import User
+from django.core.management import call_command
+from .models import User 
 
 def index(request):
     try:
+        # 1. Try to fetch users
         ids = User.objects.all()
-    except:
-        ids = [] # Fallback if table doesn't exist yet
+        # Trigger a query to see if the table exists
+        list(ids[:1]) 
+    except Exception:
+        # 2. If the table is missing, run migrate automatically
+        call_command('migrate', interactive=False)
+        ids = User.objects.all()
+        
     return render(request, 'welcome.html', {"user": ids})
